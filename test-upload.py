5646334911d4d1a5e515ee7e717d7d683d04febc
Douglas Wong <douglasbwong@gmail.com>
2017-03-23 10:55:47 -0700

    fixed test-upload

--- test-upload.py
+++ test-upload.py
@@ -2,6 +2,7 @@ import requests
 
 data = {'repositoryId': 20147, 'folderId': 30848}
 
-response = requests.get('http://172.16.20.138:9080/api/jsonws/dlapp/get-subfolder-ids', auth=('test@liferay.com', 'test'), json=data)
+response = requests.get('http://172.16.20.138:9080/api/jsonws/dlapp/get-subfolder-ids', auth=('test@liferay.com', 'test'), params=data)
 
+print(response.status_code)
 print(response.text)
