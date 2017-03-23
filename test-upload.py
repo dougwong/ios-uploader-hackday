3b0c94da159c62fb1c12b035e503eda582dfeaf9
Shane Merriss <shane.merriss@liferay.com>
2017-03-23 11:51:52 -0700

    create command line arguments and make upload into a function

--- test-upload.py
+++ test-upload.py
@@ -1,8 +1,47 @@
 import requests
 
+baseURL = "http://172.16.20.138:9080/api/jsonws/dlapp/"
+userEmail = "test@liferay.com"
+password = "test"
+command = "add-file-entry"
 data = {'repositoryId': 20147, 'folderId': 30848}
 
-response = requests.get('http://172.16.20.138:9080/api/jsonws/dlapp/get-subfolder-ids', auth=('test@liferay.com', 'test'), params=data)
+def uploadFileToLiferay(title, file):
+	data = {
+		'mimeType' : '', 
+		'description' : 'Test picture', 
+		'changeLog' : '', 
+		'sourceFileName' : '', 
+		'title' : title, 
+		'serviceContext' : {}, 
+		'repositoryId' : 20147, 
+		'folderId' : 30848 }
+	files = {
+		'file' : open(file, 'rb')
+	}
 
-print(response.status_code)
-print(response.text)
+	response = requests.post(baseURL + command, 
+		auth=(userEmail, password), 
+		params=data, 
+		files=files)
+
+	print(response.status_code)
+	print(response.text)
+
+
+if __name__ == '__main__':
+    import  argparse
+    parser = argparse.ArgumentParser()
+
+    #Required
+    parser.add_argument("-t", "--title", dest="title", type=str, help="Set the file name.")
+    parser.add_argument("-f", "--file", dest="file", type=str, help="Set the file to upload.")
+
+    parser.add_argument("command", nargs=1, choices=['upload', 'check'],  help="Specify a command. upload, check.")
+
+    args = parser.parse_args()
+
+    print('Hello')
+
+    if args.command[0] == 'upload':
+        uploadFileToLiferay(args.title, args.file)
