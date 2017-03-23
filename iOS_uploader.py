52cc18cdda1688de18c05cfc063c0d3fbe4298cc
Douglas Wong <douglas.wong@liferay.com>
2017-03-23 15:39:47 -0700

    Update iOS_uploader.py

--- iOS_uploader.py
+++ iOS_uploader.py
@@ -1,47 +1,87 @@
 import photos, requests
 from objc_util import ObjCInstance
 
-baseURL = "http://172.16.20.138:9080/api/jsonws/dlapp/"
-userEmail = "test@liferay.com"
-password = "test"
-command = "add-file-entry"
-data = {'repositoryId': 20147, 'folderId': 30848}
+base_url = "http://portal.dougtest.wedeploy.io"
+user_email = "test@liferay.com"
+password = "test1234"
+repository_id = 20147
+folder_id = 30723
+album_name = 'Hackday'
+keeper_album_name = 'Keeper'
 
-def uploadFileToLiferay(title, file):
+def upload_file(title, file):
 	data = {
-		'mimeType' : '', 
-		'description' : 'Test picture', 
-		'changeLog' : '', 
-		'sourceFileName' : '', 
-		'title' : title, 
-		'serviceContext' : {}, 
-		'repositoryId' : 20147, 
-		'folderId' : 30848 }
-	files = {
-		'file' : file
+		'mimeType': '', 
+		'description': '', 
+		'changeLog': '', 
+		'sourceFileName': '', 
+		'title': title, 
+		'serviceContext': {}, 
+		'repositoryId': repository_id, 
+		'folderId': folder_id
 	}
 
-	response = requests.post(baseURL + command, 
-		auth=(userEmail, password), 
+	files = {
+		'file': file
+	}
+
+	response = requests.post(
+		base_url + "/api/jsonws/dlapp/add-file-entry", 
+		auth=(user_email, password), 
 		params=data, 
 		files=files)
 
 	if response.status_code != 200:
-		print("Sorry, unable to upload file.") 
-		print(response.status_code)
-		print(response.text)
+		json = response.json()
+
+		if json['throwable'].startswith("com.liferay.document.library.kernel.exception.DuplicateFileEntryException"):
+			print("Sorry, unable to upload file: %s. A file with the name '%s' already exists." % (title, title))
+		else:
+			print("Sorry, unable to upload file: %s. %i - %s" % (title, response.status_code, response.text))
+	
+	return response.status_code
+
+hackday_album = None
+keeper_album = None
 
 asset_collections = photos.get_albums()
 
 for asset_collection in asset_collections:
-	if asset_collection.title == 'Hackday':
+	if asset_collection.title == album_name:
 		hackday_album = asset_collection
+		
+	if asset_collection.title == keeper_album_name:
+		keeper_album = asset_collection
 
+	if hackday_album and keeper_album:
 		break
 
-if hackday_album:
-	print(len(hackday_album.assets))
-	for asset in hackday_album.assets:
-		print(asset.local_id)
+keeper_filenames = []
+
+if keeper_album:
+	for asset in keeper_album.assets:
 		filename = str(ObjCInstance(asset).filename())
-		uploadFileToLiferay(filename, asset.get_image_data())
+
+		keeper_filenames.append(filename)
+
+if hackday_album:
+	print("%i files found in the album" % (len(hackday_album.assets)))
+
+	delete_assets = []
+
+	for asset in hackday_album.assets:
+		filename = str(ObjCInstance(asset).filename())
+
+		print("Uploading file '%s'" % (filename))
+
+		status_code = upload_file(filename, asset.get_image_data())
+
+		if status_code == 200:
+			if filename not in keeper_filenames:
+				delete_assets.append(asset)
+			else:
+				print("%s was found in the 'Keeper' album and will not be deleted" % (filename))
+
+	photos.batch_delete(delete_assets)			
+
+	print("Upload complete.")
